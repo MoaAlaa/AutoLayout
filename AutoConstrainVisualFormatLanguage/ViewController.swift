47fca448116b81b285a0dc6db638ce426d6a3610
MoaAlaa <moaalaa16@gmail.com>
2018-12-14 15:56:00 +0200

    IMplementing Auto Layout With Ancores And VFL

--- AutoConstrainVisualFormatLanguage/ViewController.swift
+++ AutoConstrainVisualFormatLanguage/ViewController.swift
@@ -9,10 +9,85 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let label1 = UILabel()
+        label1.translatesAutoresizingMaskIntoConstraints = false
+        label1.backgroundColor = UIColor.red
+        label1.text = "THESE"
+        label1.sizeToFit()
+        
+        let label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.backgroundColor = UIColor.cyan
+        label2.text = "ARE"
+        label2.sizeToFit()
+        
+        let label3 = UILabel()
+        label3.translatesAutoresizingMaskIntoConstraints = false
+        label3.backgroundColor = UIColor.yellow
+        label3.text = "SOME"
+        label3.sizeToFit()
+        
+        let label4 = UILabel()
+        label4.translatesAutoresizingMaskIntoConstraints = false
+        label4.backgroundColor = UIColor.green
+        label4.text = "AWESOME"
+        label4.sizeToFit()
+        
+        let label5 = UILabel()
+        label5.translatesAutoresizingMaskIntoConstraints = false
+        label5.backgroundColor = UIColor.orange
+        label5.text = "LABELS"
+        label5.sizeToFit()
+        
+        view.addSubview(label1)
+        view.addSubview(label2)
+        view.addSubview(label3)
+        view.addSubview(label4)
+        view.addSubview(label5)
+        
+        // Visual Format Language
+//        let viewsDictionary = ["label1": label1, "label2": label2, "label3": label3, "label4": label4, "label5": label5]
+//
+//        let metrics = ["labelHeight": 88]
+//
+//        for label in viewsDictionary.keys {
+//            // Add Visual Format Horiziontal
+//            // ( | ) Mean Streatch To The Eadg Of The View Controllers's View From The The Start To End ( | second pip)
+//            view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "H:|[\(label)]|", options: [], metrics: nil, views: viewsDictionary))
+//        }
+        
+        // Add Visual Format Veticals
+        // ] (-) [  mean space by 10 points
+        // NOTE If No ( | second pip) At the Eand That's Mean You Can Not Streatch To The End And Leave A White Spaces
+        // Every Label Will Be 88 Point Height And Tha Last Leable Will Be Greater Than Or Equals 10 Points Far Away From The Bottom
+        // - Simple Space Sign
+        // -(>=10)- Space With At Least 10 Points
+//        view.addConstraints( NSLayoutConstraint.constraints(withVisualFormat: "V:|[label1(labelHeight@999)]-[label2(label1)]-[label3(label1)]-[label4(label1)]-[label5(label1)]-(>=10)-|", options: [], metrics: metrics, views: viewsDictionary))
+        
+        // Ancores
+        var previousLable: UILabel?
+        
+        for label in [label1, label2, label3, label4, label5] {
+            label.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
+            label.heightAnchor.constraint(equalToConstant: 88).isActive = true
+            
+            if let previous = previousLable {
+                // we have a previous label – create a height constraint
+                label.topAnchor.constraint(equalTo: previous.bottomAnchor, constant: 10).isActive = true
+            }
+            
+            previousLable = label
+        }
+
+        
     }
 
 
